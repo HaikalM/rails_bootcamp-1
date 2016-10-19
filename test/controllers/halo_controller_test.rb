de556e5583553f5cb811d6945179fb7c275a90b3
Muslih <muslihzarth@gmail.com>
2016-10-19 11:53:55 +0800

    fix test unit for halo and report

--- test/controllers/halo_controller_test.rb
+++ test/controllers/halo_controller_test.rb
@@ -1,8 +1,8 @@
 require 'test_helper'
 
 class HaloControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get halo_index_url
+  test "melakukan GET halaman index" do
+    get root_url
     assert_response :success
   end
 
